c15926cb5d71bdc4fd285a055c537f398c41c788
alphaRomeo323 <alphaRomeo323dev@protonmail.com>
2023-01-27 05:40:19 +0900

    customized build configuration

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 echo "Starting snapserver and mopidy..."
 
 snapserver &
-mopidy -o "audio/output=audioresample!audioconvert!audio/x-raw,rate=48000,channels=2,format=S16LE!filesink location=/tmp/snapfifo" -o "http/hostname=0.0.0.0" -o "mpd/hostname=0.0.0.0" -o "youtube/youtube_dl_package=yt_dlp"
+mopidy -o "audio/output=audioresample!audioconvert!audio/x-raw,rate=48000,channels=2,format=S16LE!filesink location=/tmp/snapfifo" -o "http/hostname=::" -o "mpd/hostname=::" -o "local/media_dir=/opt/music"
