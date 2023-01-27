e7ea6720c14b3cd163b636d3acd53bc4cc442353
alphaRomeo323 <alphaRomeo323dev@protonmail.com>
2023-01-27 15:43:32 +0900

    removed strong dependencies

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-echo "Scanning local files"
-mkdir -p ~/.config/mopidy
-echo -e "[local]\n  media_dir=/music" > ~/.config/mopidy/mopidy.conf
-mopidy local scan
+### because of strong depends on python3.10, this script is failed to mount.
+# echo "Scanning local files"
+# mkdir -p ~/.config/mopidy
+# echo -e "[local]\n  media_dir=/music" > ~/.config/mopidy/mopidy.conf
+# mopidy local scan
 
 echo "Starting snapserver and mopidy..."
 
 snapserver &
-mopidy -o "audio/output=audioresample!audioconvert!audio/x-raw,rate=48000,channels=2,format=S16LE!filesink location=/tmp/snapfifo" -o "http/hostname=::" -o "mpd/hostname=::" -o "local/media_dir=/music"
+mopidy -o "audio/output=audioresample!audioconvert!audio/x-raw,rate=48000,channels=2,format=S16LE!filesink location=/tmp/snapfifo" -o "http/hostname=::" -o "mpd/hostname=::" -o "file/media_dirs=/music|Music"
