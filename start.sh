d9817e62f38e094174c00b88349f83b19299a63e
alphaRomeo323 <alphaRomeo323dev@protonmail.com>
2023-01-27 16:20:00 +0900

    fixed bugs

--- start.sh
+++ start.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-### because of strong depends on python3.10, this script is failed to mount.
-# echo "Scanning local files"
-# mkdir -p ~/.config/mopidy
-# echo -e "[local]\n  media_dir=/music" > ~/.config/mopidy/mopidy.conf
-# mopidy local scan
-
-echo "test"
-ls -l /music
+echo "Scanning local files"
+mkdir -p ~/.config/mopidy
+echo -e "[local]\n  media_dir=/music" > ~/.config/mopidy/mopidy.conf
+mopidy local scan
 
 echo "Starting snapserver and mopidy..."
 
 snapserver &
-mopidy -o "audio/output=audioresample!audioconvert!audio/x-raw,rate=48000,channels=2,format=S16LE!filesink location=/tmp/snapfifo" -o "http/hostname=::" -o "mpd/hostname=::" -o "file/media_dirs=/music|Music"
+mopidy -o "audio/output=audioresample!audioconvert!audio/x-raw,rate=48000,channels=2,format=S16LE!filesink location=/tmp/snapfifo" -o "http/hostname=::" -o "mpd/hostname=::" -o "file/enabled=false" -o "local/media_dir=/music"
